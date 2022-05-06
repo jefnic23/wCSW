ac8820cbb241d6b1ab5ce318da33908930456197
Jeff <jefnic23@gmail.com>
2022-05-05 22:21:37 -0400

    rename wCSW to wES

--- wes.py
+++ wes.py
@@ -49,46 +49,46 @@ if __name__ == '__main__':
             print('Not a valid year')
 
     # initialize progress bar
-    pbar = tqdm(total=70)
+    pbar = tqdm(total=7)
 
     while True:
         # get player id data and read into a pandas dataframe
         url = "https://raw.githubusercontent.com/chadwickbureau/register/master/data/people.csv"
         lookup = pd.read_csv(url, index_col="key_mlbam")
         lookup['name'] = lookup['name_first'] + ' ' + lookup['name_last']
-        pbar.update(10)
+        pbar.update(1)
 
         # locate savant data and read into pandas dataframe
         dir = os.path.abspath(f'../baseball_savant/data/savant_{year}.csv')
         df = pd.read_csv(dir)
-        pbar.update(10)
+        pbar.update(1)
 
         # data cleanup
         df['csw'] = np.where(df['description'].isin(['swinging_strike', 'swinging_strike_blocked', 'called_strike', 'foul_tip']), 1, 0)
         df['count'] = df['balls'].astype(str) + '-' + df['strikes'].astype(str)
         df = df.loc[df['count'] != '4-2']     # for some reason there's a 4-2 count
-        pbar.update(10)
+        pbar.update(1)
 
         # get csw weights by count
         weights = getCountWeights(df)
-        pbar.update(10)
+        pbar.update(1)
 
-        # group batters by count and get total wcsw
+        # group batters by count and get total wES
         df = df.groupby(['pitcher', 'count']).agg(total_pitches=('at_bat_number', 'count'), total_csw=('csw', 'sum')).reset_index()
-        df['wCSW'] = df['count'].map(weightCount) * df['total_csw']
-        pbar.update(10)
+        df['wES'] = df['count'].map(weightCount) * df['total_csw']
+        pbar.update(1)
         
-        # group batters again to get wcsw rate
-        df = df.groupby('pitcher').agg({'total_pitches': 'sum', 'wCSW': 'sum'})
-        df['wCSW'] = df['wCSW'] / df['total_pitches']
+        # group batters again to get wES rate
+        df = df.groupby('pitcher').agg({'total_pitches': 'sum', 'wES': 'sum'})
+        df['wES'] = df['wES'] / df['total_pitches']
         df.index = df.index.map(name)
-        pbar.update(10)
+        pbar.update(1)
 
         # make directory to house savant data
         if not os.path.exists('data'):
             os.mkdir('data')
 
-        df.to_csv(f"data/wCSW_{year}.csv")
-        pbar.update(10)
+        df.to_csv(f"data/wES_{year}.csv")
+        pbar.update(1)
 
         break
